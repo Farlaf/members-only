3e98397e0c478b98fdaf71880731b6c2c05583a2
Farlaf <slepuhovoleg@gmail.com>
2023-02-20 13:27:36 +0400

    add post model and blank controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
   root 'pages#home'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
