79ad362db797bc661642cf48af0984baf488b8ce
Farlaf <slepuhovoleg@gmail.com>
2023-02-20 15:03:26 +0400

    add form and posts list

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,24 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
